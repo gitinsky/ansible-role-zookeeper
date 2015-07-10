7a3698d627566b4693af3a9c12ee4781e89520c7
Alex Chistyakov <alexclear@gmail.com>
2015-07-10 09:42:45 +0300

    Make user, path and ports configurable

--- templates/zookeeper/eye.rb
+++ templates/zookeeper/eye.rb
@@ -1,12 +1,12 @@
-Eye.application 'zookeeper' do
+Eye.application 'zookeeper-{{ env_name }}' do
   working_dir '/etc/eye'
-  stdall '/var/log/eye/zookeeper-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/zookeeper-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :zookeeper do
-    pid_file '/var/lib/zookeeper/zookeeper_server.pid'
-    start_command 'sudo -u zookeeper /opt/zookeeper/bin/zkServer.sh start'
+  process :zookeeper_{{ env_name }} do
+    pid_file '{{ zookeeper_var_prefix }}/zookeeper_server.pid'
+    start_command 'sudo -u {{ zookeeper_user }} {{ zookeeper_distr_prefix }}/bin/zkServer.sh start'
 
     start_timeout 10.seconds
     stop_timeout 5.seconds
--- templates/zookeeper/zookeeper-env.sh
+++ templates/zookeeper/zookeeper-env.sh
@@ -1,4 +1,4 @@
-ZOO_LOG_DIR=/var/log/zookeeper
+ZOO_LOG_DIR={{ zookeeper_log_prefix }}
 export ZOO_LOG_DIR
 JAVA_HOME=/usr/java/jdk1.8.0_40
 export JAVA_HOME
