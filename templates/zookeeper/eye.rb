327699f75a4131623f813626a4d2c513be1d8b57
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 17:33:47 +0300

    Use uid

--- templates/zookeeper/eye.rb
+++ templates/zookeeper/eye.rb
@@ -3,10 +3,11 @@ Eye.application 'zookeeper-{{ env_name }}' do
   stdall '/var/log/eye/zookeeper-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ zookeeper_user }}"
 
   process :zookeeper_{{ env_name }} do
     pid_file '{{ zookeeper_var_prefix }}/zookeeper_server.pid'
-    start_command 'sudo -u {{ zookeeper_user }} {{ zookeeper_distr_prefix }}/bin/zkServer.sh start'
+    start_command '{{ zookeeper_distr_prefix }}/bin/zkServer.sh start'
 
     start_timeout 10.seconds
     stop_timeout 5.seconds
